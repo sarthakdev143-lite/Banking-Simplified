1373d782fcb4da0532e19ed8bd64eee520f6e73f
sarthakdev143-lite <sarthakdev143.official@gmail.com>
2024-09-22 13:21:41 +0530

    "Updated CursorFollower import and removed unnecessary whitespace; removed performance measurement code"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ import CursorFollower from './CursorFollower';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CursorFollower/>
+    <CursorFollower />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -32,7 +32,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);

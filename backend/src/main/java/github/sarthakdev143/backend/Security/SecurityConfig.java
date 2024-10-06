3c84a22c1d9ff1f6b427b656f0b6a56c69755e3b
sarthakdev143 <sarthakdev143.official@gmail.com>
2024-10-06 22:17:37 +0530

    "Commented out Spring Security configuration and related classes"

--- backend/src/main/java/github/sarthakdev143/backend/Security/SecurityConfig.java
+++ backend/src/main/java/github/sarthakdev143/backend/Security/SecurityConfig.java
@@ -1,54 +1,54 @@
-package github.sarthakdev143.backend.Security;
+// package github.sarthakdev143.backend.Security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// // import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
 
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//     @Bean
+//     public static PasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         http
+//                 .authorizeHttpRequests((requests) -> requests
 
-                        .requestMatchers("/api/create/**").permitAll()
-                        .requestMatchers("/api/login/**").permitAll()
+//                         .requestMatchers("/api/create/**").permitAll()
+//                         .requestMatchers("/api/login/**").permitAll()
 
-                        .requestMatchers("/api/account/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
+//                         .requestMatchers("/api/account/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+//                         .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
         
-                        .anyRequest().authenticated())
+//                         .anyRequest().authenticated())
 
-                .formLogin((form) -> form
-                        .loginPage("/api/login")
-                        .loginProcessingUrl("/api/login")
-                        .defaultSuccessUrl("/")
-                        .usernameParameter("email")
-                        .passwordParameter("password")
-                        .permitAll())
-                .logout(obj -> obj.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true).deleteCookies("JESSIONID"));
-        http.exceptionHandling((hi) -> hi.accessDeniedPage("/access-denied"));
-        System.out.println("\n\nHello from Security Config\n\n");
-        return http.build();
-    }
+//                 .formLogin((form) -> form
+//                         .loginPage("/api/login")
+//                         .loginProcessingUrl("/api/login")
+//                         .defaultSuccessUrl("/")
+//                         .usernameParameter("email")
+//                         .passwordParameter("password")
+//                         .permitAll())
+//                 .logout(obj -> obj.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                         .logoutSuccessUrl("/")
+//                         .invalidateHttpSession(true).deleteCookies("JESSIONID"));
+//         http.exceptionHandling((hi) -> hi.accessDeniedPage("/access-denied"));
+//         System.out.println("\n\nHello from Security Config\n\n");
+//         return http.build();
+//     }
 
-    // @Bean
-    // public WebSecurityCustomizer webSecurityCustomizer() {
-    //     return (web) -> web.ignoring().requestMatchers("/");
-    // }
-}
+//     // @Bean
+//     // public WebSecurityCustomizer webSecurityCustomizer() {
+//     //     return (web) -> web.ignoring().requestMatchers("/");
+//     // }
+// }
--- backend/src/main/java/github/sarthakdev143/backend/Security/TbConstants.java
+++ backend/src/main/java/github/sarthakdev143/backend/Security/TbConstants.java
@@ -1,14 +1,14 @@
-package github.sarthakdev143.backend.Security;
+// package github.sarthakdev143.backend.Security;
 
-public class TbConstants {
+// public class TbConstants {
 
-    public static interface Roles {
+//     public static interface Roles {
 
-        // ROLE_Admin
-        final Long ADMIN = 1l;
+//         // ROLE_Admin
+//         final Long ADMIN = 1l;
 
-        // ROLE_USER
-        final Long USER = 2l;
+//         // ROLE_USER
+//         final Long USER = 2l;
 
-    }
-}
+//     }
+// }

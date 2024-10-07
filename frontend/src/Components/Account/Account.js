3081bf9725ab4696906e4572ea411c08c6b7b82c
sarthakdev143 <sarthakdev143.official@gmail.com>
2024-10-07 07:23:53 +0530

    "Updated Account.js: changed import to include useEffect, replaced useLayoutEffect with useEffect"

--- frontend/src/Components/Account/Account.js
+++ frontend/src/Components/Account/Account.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Account = () => {
   
-  useLayoutEffect(() => {
+  useEffect(() => {
     alert("This Part is in Under Construction.")
     window.location.href = '/';
   }, [])

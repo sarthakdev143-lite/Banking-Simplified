18e411087c92c0a54d7bba79d225aca02dfff858
sarthakdev143 <sarthakdev143.official@gmail.com>
2024-10-07 06:31:39 +0530

    Handling Account url

--- frontend/src/Components/Account/Account.js
+++ frontend/src/Components/Account/Account.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Account = () => {
+  
+  useLayoutEffect(() => {
+    alert("This Part is in Under Construction.")
+    window.location.href = '/';
+  }, [])
+
   return (
     <div>
       <img src='https://images.unsplash.com/photo-1577915207354-8f266a3fc016?auto=format&fit=crop&w=1470&q=80' alt='Background Not Found' />
